8a529cf18227778660fa27cddf6f8c0527ba45f6
rahabyin <rahabnwanja@gmail.com>
2026-03-27 09:16:43 +0300

    js arrays commit

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,31 @@
 // Write your code here
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+function addProduct(newProduct) {
+  products.push(newProduct);
+  return products;
+}
+
+addProduct("Mouse");
+
+function updateProductName(position, newName) {
+  if (position >= 0 && position < products.length) {
+    products[position] = newName;
+    console.log("Product list updated successfully!");
+  } else {
+    console.log("Error: That position does not exist.");
+  }
+  return products;
+}
+
+function removeLastProduct() {
+  products.pop();
+  console.log(products);
+}
 
 
 // Export the necessary parts for testing
