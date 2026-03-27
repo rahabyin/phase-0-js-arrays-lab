b07fbf0ad6ea080d8ea41f79c0a1541ed4fe5194
rahabyin <rahabnwanja@gmail.com>
2026-03-27 09:24:08 +0300

    js arrays commit

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,3 +1,4 @@
+// Write your code here
 let products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
 function logFirstProduct() {
@@ -23,3 +24,13 @@ function removeLastProduct() {
   products.pop();
   console.log(products);
 }
+
+
+// Export the necessary parts for testing
+module.exports = {
+  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
+  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
+  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
+  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
+  products
+};
